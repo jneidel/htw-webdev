1ddb5807203e05fabc244762df84b6f98244ab44
zink-l <leon.enzenberger@protonmail.com>
2020-11-24 21:59:14 +0100

    Create first draft of landing page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require( "express" );
 const router = express.Router();
 
-router.get( "/", ( req, res ) => res.render( "welcome", { title: "wd-todo" } ) );
+router.get( "/", ( req, res ) => res.render( "landing" ) );
+router.get( "/welcome", ( req, res ) => res.render( "welcome", { title: "wd-todo" } ) );
 
 module.exports = router;
