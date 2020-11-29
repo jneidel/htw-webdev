a259495b3b38f6c3a3aa9fffbd5e2df54585b015
zink-l <leon.enzenberger@protonmail.com>
2020-11-29 15:11:53 +0100

    Add background-picture and do minor changes
    - The "What to do?" logo now has its own class
    - The title of the landing-page has changed to "What to do?"
    - The color of the header has changed to white instead of transparent

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require( "express" );
 const router = express.Router();
 
-router.get( "/", ( req, res ) => res.render( "landing", { title: "landing-page"} ) );
+router.get( "/", ( req, res ) => res.render( "landing", { title: "What to do?"} ) );
 router.get( "/welcome", ( req, res ) => res.render( "welcome", { title: "wd-todo" } ) );
 
 module.exports = router;
