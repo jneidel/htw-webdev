c7aedf67b894b8112f29b5198d862c06c4ad249b
zink-l <leon.enzenberger@protonmail.com>
2020-12-28 23:04:50 +0100

    Create login page

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.get( "/", ( req, res ) => res.render( "landing", { title: "What to do?"} ) );
 router.get( "/welcome", ( req, res ) => res.render( "welcome", { title: "wd-todo" } ) );
+router.get( "/login", ( req, res ) => res.render( "login", { title: "login" } ) );
 
 module.exports = router;
