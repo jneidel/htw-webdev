e6232a81a7ace0dece603e02ab893eb58c332000
zink-l <leon.enzenberger@protonmail.com>
2021-01-17 11:52:00 +0100

    Make rudimentary home-page

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.get( "/", ( req, res ) => res.render( "landing", { title: "What to do?"} ) );
 router.get( "/welcome", ( req, res ) => res.render( "welcome", { title: "wd-todo" } ) );
 router.get( "/login", ( req, res ) => res.render( "login", { title: "login" } ) );
+router.get( "/home", ( req, res ) => res.render( "home", { data: { title: "home", name:"Leon" } } ) );
 
 module.exports = router;
