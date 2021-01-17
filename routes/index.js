a86b87e38c9c6650c96ff9154a87efcfaf0c89d6
zink-l <leon.enzenberger@protonmail.com>
2021-01-17 11:55:32 +0100

    Add register-site

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get( "/", ( req, res ) => res.render( "landing", { title: "What to do?"} ) );
 router.get( "/welcome", ( req, res ) => res.render( "welcome", { title: "wd-todo" } ) );
 router.get( "/login", ( req, res ) => res.render( "login", { title: "login" } ) );
+router.get( "/register", ( req, res ) => res.render( "register", { title: "register" } ) );
 router.get( "/home", ( req, res ) => res.render( "home", { data: { title: "home", name:"Leon" } } ) );
 
 module.exports = router;
