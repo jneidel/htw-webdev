a5749292c9cf780e1aa5a88b5fdad49ad03cf8f7
zink-l <leon.enzenberger@protonmail.com>
2021-01-17 12:00:33 +0100

    Change parameters for home

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ router.get( "/", ( req, res ) => res.render( "landing", { title: "What to do?"} 
 router.get( "/welcome", ( req, res ) => res.render( "welcome", { title: "wd-todo" } ) );
 router.get( "/login", ( req, res ) => res.render( "login", { title: "login" } ) );
 router.get( "/register", ( req, res ) => res.render( "register", { title: "register" } ) );
-router.get( "/home", ( req, res ) => res.render( "home", { data: { title: "home", name:"Leon" } } ) );
+router.get( "/home", ( req, res ) => res.render( "home", { title: "home", name:"Leon" } ) );
 
 module.exports = router;
