0d32ff2f8f2107abcab5d7a0e7791d9739b3ef9f
Jonathan Neidel <jneidel@jneidel.com>
2021-01-23 12:00:45 +0100

    Lint app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,51 +1,46 @@
 const App = {
   data() {
     return {
-      todos: [
-				{ id:123, completed:false, text: "big chungus" },
-			],
       newTodo: "",
+      todos  : [ // load from db
+        { id: 123, completed: false, text: "big chungus" },
+      ],
     };
   },
-  methods: {
-		addTodo: function () {
-      console.log( "adding" )
-			var value = this.newTodo && this.newTodo.trim();
-			if (!value) {
-				return;
-			}
-			this.todos.push({ id: this.todos.length + 1, title: value, completed: false });
-			this.newTodo = '';
-		},
 
-		removeTodo: function (todo) {
-      console.log( "removing" )
-			var index = this.todos.indexOf(todo);
-			this.todos.splice(index, 1);
-		},
-
-		editTodo: function (todo) {
-      console.log( "editing" )
-			this.beforeEditCache = todo.title;
-			this.editedTodo = todo;
-		},
-
-		doneEdit: function (todo) {
-      console.log( "done editing" )
-			if (!this.editedTodo) {
-				return;
-			}
-			this.editedTodo = null;
-			todo.title = todo.title.trim();
-			if (!todo.title) {
-				this.removeTodo(todo);
-			}
-		},
+  computed: {
+    remaining() {
+      return this.todos.filter( t => !t.completed ).length;
+    },
   },
-  mounted() {
-    // setInterval( () => {
-    //   console.log( this.todos )
-    // }, 1000 );
+  methods: {
+    pluralize( word, count ) {
+      return word + ( count === 1 ? "" : "s" );
+    },
+    addTodo() {
+      const value = this.newTodo && this.newTodo.trim();
+      console.log( `Adding: ${  value}` );
+      if ( !value ) return;
+      this.todos.push( { id: this.todos.length + 1, text: value, completed: false } ); // put tempId, to be replaced w/ uuid from db
+      this.newTodo = "";
+      // sync w/ db
+    },
+    removeTodo( todo ) {
+      const index = this.todos.indexOf( todo );
+      console.log( `Removing: ${  this.todos[index].text}` );
+      this.todos.splice( index, 1 );
+      // sync w/ db
+    },
+    edit( todo ) {
+      console.log( `Done editing: ${  todo.text}` );
+      todo.text = todo.text.trim();
+      if ( !todo.text ) this.removeTodo( todo );
+      // sync w/ db
+    },
+    removeCompleted() {
+      this.todos = this.todos.filter( t => !t.completed );
+      // sync w/ db
+    },
   },
 };
 
