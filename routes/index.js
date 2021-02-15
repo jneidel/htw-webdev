7640729a6056249c7ea0daae3961b38369a66504
zink-l <leon.enzenberger@protonmail.com>
2021-02-15 14:44:52 +0100

    Remove unused and redundant routes

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,9 @@ const { checkAuthenticated } = passport_config;
 const { checkNotAuthenticated } = passport_config;
 
 router.get( "/", ( req, res ) => res.render( "landing", { title: "What to do?" } ) );
-router.get( "/welcome", ( req, res ) => res.render( "welcome", { title: "wd-todo" } ) );
 router.get( "/login", checkNotAuthenticated, ( req, res ) => res.render( "login", { title: "login" } ) );
 router.get( "/register", checkNotAuthenticated, ( req, res ) => res.render( "register", { title: "register" } ) );
 router.get( "/home", checkAuthenticated, ( req, res ) => res.render( "home", { title: "home", name: "Guest" } ) );
-router.get( "/", ( req, res ) => res.render( "landing", { title: "What to do?" } ) );
 router.get( "/app", ( req, res ) => res.render( "app", { title: "App" } ) );
 
 module.exports = router;
